873ee1156ac6498ea556bfecebe7e5ab82f134c8
binymarcus <48927315+binymarcus@users.noreply.github.com>
2019-11-14 16:05:34 +0200

    added the guest request class, issues with tostring

--- dotNet5780_02_9916_08225/dotNet5780_02_9916_08225/Program.cs
+++ dotNet5780_02_9916_08225/dotNet5780_02_9916_08225/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using dotNet5780_02_9916_08225.Properties;
 namespace dotNet5780_02_9916_08225
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GuestRequest guesty = new GuestRequest();
+            Console.WriteLine(guesty.ToString());
+            Console.Read();
+            
         }
     }
 }
---
+++ dotNet5780_02_9916_08225/dotNet5780_02_9916_08225/Properties/GuestRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_02_9916_08225.Properties
+{
+    class GuestRequest
+    {
+        public int[] EntryDate = new int[3];
+        public int[] ReleaseDate = new int[3];
+        public bool IsApproved;
+
+        public GuestRequest(int[] entryDate, int[] releaseDate, bool isApproved)
+        {
+            EntryDate = entryDate;
+            ReleaseDate = releaseDate;
+            IsApproved = isApproved;
+        }
+
+        public GuestRequest()
+        { 
+            EntryDate = new int[3]{ 1, 1, 2020 };
+            ReleaseDate = new int[3]{ 2, 1, 2020 };
+            IsApproved = false;
+        }
+        public override string ToString()
+        {
+                Console.WriteLine("Entry Date: {0}.{1}.{2} Release Date: {4} {5}.{6} IsApproved:{7}",EntryDate[0] , EntryDate[1] , EntryDate[2] , ReleaseDate[0] , ReleaseDate[1], ReleaseDate[2], 
+            IsApproved);
+            return base.ToString();
+        }
+    }
+}
