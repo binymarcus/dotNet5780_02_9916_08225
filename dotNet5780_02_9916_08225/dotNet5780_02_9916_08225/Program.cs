d32acea054c36014f24c51c0af0f4b83d24c1344
binymarcus <48927315+binymarcus@users.noreply.github.com>
2019-11-14 15:03:42 +0200

    created the program

---
+++ dotNet5780_02_9916_08225/dotNet5780_02_9916_08225/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_02_9916_08225
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
